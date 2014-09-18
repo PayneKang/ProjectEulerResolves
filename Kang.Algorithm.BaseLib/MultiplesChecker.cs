28eb26939054a5851175e46c04eab7bc81f12173
Payne Kang <daitou0322@tom.com>
2014-09-18 10:18:29 +0800

    Problem001

---
+++ Kang.Algorithm.BaseLib/MultiplesChecker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kang.Algorithm.BaseLib
+{
+    public class MultiplesChecker
+    {
+        private MultiplesChecker() { }
+        public MultiplesChecker(int product, int[] multipliers)
+        {
+            this.Product = product;
+            this.Multipliers = multipliers;
+        }
+        public int Product { get; private set; }
+        public int[] Multipliers { get; private set; }
+        public bool Check()
+        {
+            int tempProduct = this.Product;
+            foreach (int multiplier in Multipliers)
+            {
+                while (tempProduct % multiplier == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Problem001/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Kang.Algorithm.BaseLib;
+
+namespace Problem001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] multiples = new int[] {3,5};
+            int sum = 0;
+            for(int i = 3; i < 1000; i ++){
+                MultiplesChecker checker = new MultiplesChecker(i, multiples);
+                if (checker.Check())
+                {
+                    Console.WriteLine(i);
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
