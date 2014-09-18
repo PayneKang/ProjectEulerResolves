25eba07a721f01c45ab2a2e5435f73380dfa8df8
Payne Kang <daitou0322@tom.com>
2014-09-18 11:53:57 +0800

    Problem006

---
+++ Problem006/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+            sum = sum * sum;
+            Console.WriteLine(sum);
+            for (int i = 1; i <= 100; i++)
+            {
+                sum = sum - i * i;
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
