f0f1708e0149479cb6282dd6b1888e7bad276a83
Payne Kang <daitou0322@tom.com>
2014-09-18 10:29:33 +0800

    Problem002

---
+++ Kang.Algorithm.BaseLib/FibonacciGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kang.Algorithm.BaseLib
+{
+    /// <summary>
+    /// 斐波那契额数列生成器
+    /// 每次生成下一个斐波那契数
+    /// 从3开始
+    /// </summary>
+    public class FibonacciGenerator
+    {
+        public FibonacciGenerator(){}
+        private int firstNum = 1;
+        private int secondNum = 2;
+        public int Next()
+        {
+            secondNum = firstNum + secondNum;
+            firstNum = secondNum - firstNum;
+            return secondNum;
+        }
+    }
+}
---
+++ Problem002/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Kang.Algorithm.BaseLib;
+
+namespace Problem002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FibonacciGenerator generator = new FibonacciGenerator();
+            int currentNum = 3;
+            int sum = 2;
+            while(currentNum < 4000000)
+            {
+                currentNum = generator.Next();
+                Console.Write(currentNum + " ,");
+                if (currentNum % 2 == 0)
+                    sum += currentNum;
+            }
+            Console.Write("\r\nsum = " + sum);
+            Console.ReadLine();
+        }
+    }
+}
