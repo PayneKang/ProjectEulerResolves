221de28bbdd4878eed22134d591ffadb552bacdb
Payne Kang <daitou0322@tom.com>
2014-09-18 11:38:59 +0800

    Problem005

---
+++ Problem005/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] divNum = { 3, 4, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+            int i = 20;
+            bool found = true;
+            while (true)
+            {
+                found = true;
+                foreach (int div in divNum)
+                {
+                    if (i % div != 0)
+                    {
+                        found = false;
+                        break;
+                    }
+                }
+                if (found)
+                    break;
+                i += 20;
+            }
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+}
