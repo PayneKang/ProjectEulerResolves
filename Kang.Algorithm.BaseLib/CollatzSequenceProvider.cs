c139b090e3cf4d4bcfa167d070881819f4ab2606
Payne Kang <daitou0322@tom.com>
2014-09-30 15:38:41 +0800

    Problem014

---
+++ Kang.Algorithm.BaseLib/CollatzSequenceProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kang.Algorithm.BaseLib
+{
+    public class CollatzSequenceProvider
+    {
+        public long Start { get; private set; }
+        private long currentNum;
+        public CollatzSequenceProvider() { }
+        public void setStartNum(long start)
+        {
+            this.Start = start;
+        }
+        public void DoBuildSequence()
+        {
+
+        }
+        public int DoCalculateSequenceLength()
+        {
+            int length = 1;
+            currentNum = this.Start;
+            while (currentNum > 1)
+            {
+                currentNum = Arithmetic(currentNum);
+                length++;
+            }
+            return length;
+        }
+        private long Arithmetic(long num)
+        {
+            if (num % 2 == 0)
+                return num / 2;
+            return 3 * num + 1;
+        }
+    }
+}
---
+++ Problem014/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Kang.Algorithm.BaseLib;
+using System.Diagnostics;
+
+namespace Problem014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CollatzSequenceProvider csp = new CollatzSequenceProvider();
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            int max = 0;
+            long startNum = 0;
+            for (long i = 1000000; i > 1; i--)
+            {
+                csp.setStartNum(i);
+                int rlt = csp.DoCalculateSequenceLength();
+                if (rlt > max)
+                {
+                    max = rlt;
+                    startNum = i;
+                }
+            }
+            sw.Stop();
+            Console.Out.WriteLine(sw.ElapsedTicks + " len : " + max + " start number : " + startNum);
+            Console.Read();
+        }
+    }
+}
