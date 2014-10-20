9f1eaaa5a35727501fc39adcbe1a11ac22a38c23
Payne Kang <daitou0322@tom.com>
2014-10-20 17:24:24 +0800

    Problem024_unsolved

---
+++ Problem024/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem024
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Permutation(new List<int>(), new List<int>() { 0, 1, 2 });
+            Console.ReadLine();
+        }
+        static void Permutation(List<int> preList, List<int> keys)
+        {
+            List<int> curPreList = new List<int>();
+            curPreList.AddRange(preList);
+            if (keys.Count == 1)
+            {
+                foreach (int item in preList)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine(keys[0]);
+                return;
+            }
+            for (int i = 0; i < keys.Count; i++)
+            {
+                int key = keys[i];
+                List<int> otherKeys = new List<int>();
+                otherKeys.AddRange(keys);
+                otherKeys.RemoveAt(i);
+                curPreList.Add(key);
+                Permutation(curPreList, otherKeys);
+            }
+        }
+    }
+}
